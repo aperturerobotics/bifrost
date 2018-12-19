b5d32b7662a1977db127a9c33ed52b8d85671118
Christian Stewart <christian@paral.in>
2018-12-18 22:32:13 -0800

    feat: add keypem encoding for public keys
    Signed-off-by: Christian Stewart <christian@paral.in>

--- keypem/keypem.go
+++ keypem/keypem.go
@@ -8,8 +8,11 @@ import (
 	"github.com/libp2p/go-libp2p-crypto"
 )
 
-// PemType is the expected header type on private keys.
-const PemType = "LIBP2P PRIVATE KEY"
+// PrivPemType is the expected header type on private keys.
+const PrivPemType = "LIBP2P PRIVATE KEY"
+
+// PubPemType is the expected header type on public keys.
+const PubPemType = "LIBP2P PUBLIC KEY"
 
 // ParsePrivKeyPem parses a private key in pem format.
 // If none is found returns nil
@@ -19,7 +22,7 @@ func ParsePrivKeyPem(pemDat []byte) (crypto.PrivKey, error) {
 		return nil, nil
 	}
 
-	if b.Type != PemType {
+	if b.Type != PrivPemType {
 		return nil, errors.New("unexpected pem type for private key")
 	}
 
@@ -34,7 +37,35 @@ func MarshalPrivKeyPem(key crypto.PrivKey) ([]byte, error) {
 	}
 
 	return pem.EncodeToMemory(&pem.Block{
-		Type:  PemType,
+		Type:  PrivPemType,
+		Bytes: dat,
+	}), nil
+}
+
+// ParsePubKeyPem parses a public key in pem format.
+// If none is found returns nil
+func ParsePubKeyPem(pemDat []byte) (crypto.PubKey, error) {
+	b, _ := pem.Decode(pemDat)
+	if b == nil {
+		return nil, nil
+	}
+
+	if b.Type != PubPemType {
+		return nil, errors.New("unexpected pem type for public key")
+	}
+
+	return crypto.UnmarshalPublicKey(b.Bytes)
+}
+
+// MarshalPubKeyPem marshals a public key to pem.
+func MarshalPubKeyPem(key crypto.PubKey) ([]byte, error) {
+	dat, err := crypto.MarshalPublicKey(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return pem.EncodeToMemory(&pem.Block{
+		Type:  PubPemType,
 		Bytes: dat,
 	}), nil
 }
