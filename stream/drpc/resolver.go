90b28aab2a363d34daae399185cbbe60d2d6e765
Christian Stewart <christian@paral.in>
2022-01-05 06:31:42 -0800

    feat(drpc): add mounted stream resolver
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ stream/drpc/resolver.go
@@ -0,0 +1,34 @@
+package stream_drpc
+
+import (
+	"context"
+
+	"github.com/aperturerobotics/bifrost/link"
+	"github.com/aperturerobotics/controllerbus/directive"
+)
+
+// mountedStreamResolver resolves HandleMountedStream.
+type mountedStreamResolver struct {
+	c *Server
+}
+
+func newMountedStreamResolver(c *Server) *mountedStreamResolver {
+	return &mountedStreamResolver{c: c}
+}
+
+// Resolve resolves the values, emitting them to the handler.
+// The resolver may be canceled and restarted multiple times.
+// Any fatal error resolving the value is returned.
+// The resolver will not be retried after returning an error.
+// Values will be maintained from the previous call.
+func (r *mountedStreamResolver) Resolve(
+	ctx context.Context,
+	handler directive.ResolverHandler,
+) error {
+	var val link.MountedStreamHandler = r.c
+	_, _ = handler.AddValue(val)
+	return nil
+}
+
+// _ is a type assertion
+var _ directive.Resolver = ((*mountedStreamResolver)(nil))
---
+++ stream/drpc/server.go
@@ -0,0 +1,139 @@
+package stream_drpc
+
+import (
+	"context"
+
+	"github.com/aperturerobotics/bifrost/link"
+	"github.com/aperturerobotics/bifrost/protocol"
+	"github.com/aperturerobotics/controllerbus/bus"
+	"github.com/aperturerobotics/controllerbus/directive"
+
+	"storj.io/drpc"
+	"storj.io/drpc/drpcmux"
+	"storj.io/drpc/drpcserver"
+)
+
+// RegisterFn is a callback to register apis to a mux.
+type RegisterFn func(mux drpc.Mux) error
+
+// Server handles incoming streams for a peer id.
+type Server struct {
+	// b is the bus
+	b bus.Bus
+	// drpcOpts are opts passed to drpc
+	drpcOpts *DrpcOpts
+	// protocolIDs is list of protocol id to listen on.
+	// cannot be empty
+	protocolIDs []protocol.ID
+	// peerIDs is the list of local peer ids to listen on
+	// if empty, allows any
+	peerIDs []string
+
+	// mux is the drpc mux
+	mux *drpcmux.Mux
+	// server is the drpc server
+	server *drpcserver.Server
+}
+
+// NewServer constructs a common drpc controller.
+// If peerIDs and/or domainIDs are empty, matches any.
+func NewServer(
+	b bus.Bus,
+	opts *DrpcOpts,
+	protocolIDs []protocol.ID,
+	peerIDs []string,
+	registerFns []RegisterFn,
+) (*Server, error) {
+	mux := drpcmux.New()
+	for _, rf := range registerFns {
+		if err := rf(mux); err != nil {
+			return nil, err
+		}
+	}
+
+	server := drpcserver.New(mux)
+	return &Server{
+		b:           b,
+		drpcOpts:    opts,
+		protocolIDs: protocolIDs,
+		peerIDs:     peerIDs,
+
+		mux:    mux,
+		server: server,
+	}, nil
+}
+
+// HandleDirective asks if the handler can resolve the directive.
+// If it can, it returns a resolver. If not, returns nil.
+func (s *Server) HandleDirective(ctx context.Context, di directive.Instance) (directive.Resolver, error) {
+	dir := di.GetDirective()
+	switch d := dir.(type) {
+	case link.HandleMountedStream:
+		return s.ResolveHandleMountedStream(ctx, di, d)
+	}
+
+	return nil, nil
+}
+
+// ResolveHandleMountedStream resolves a HandleMountedStream directive by dialing a target.
+func (s *Server) ResolveHandleMountedStream(
+	ctx context.Context,
+	di directive.Instance,
+	dir link.HandleMountedStream,
+) (directive.Resolver, error) {
+	inProtocol := dir.HandleMountedStreamProtocolID()
+	var match bool
+	for _, pr := range s.protocolIDs {
+		if pr == inProtocol {
+			match = true
+			break
+		}
+	}
+	if !match {
+		return nil, nil
+	}
+
+	inPeerID := dir.HandleMountedStreamLocalPeerID()
+	inPeerIDPretty := inPeerID.Pretty()
+	if len(s.peerIDs) != 0 {
+		match = false
+		for _, pid := range s.peerIDs {
+			if pid == inPeerIDPretty {
+				match = true
+			}
+		}
+		if !match {
+			return nil, nil
+		}
+	}
+
+	return newMountedStreamResolver(s), nil
+}
+
+// HandleMountedStream handles an incoming mounted stream.
+// Any returned error indicates the stream should be closed.
+// This function should return as soon as possible, and start
+// additional goroutines to manage the lifecycle of the stream.
+// Typically EstablishLink is asserted in HandleMountedStream.
+func (s *Server) HandleMountedStream(ctx context.Context, ms link.MountedStream) error {
+	// keep the link open
+	_, elRef, err := s.b.AddDirective(
+		link.NewEstablishLinkWithPeer(ms.GetLink().GetLocalPeer(), ms.GetPeerID()),
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+	go func() {
+		subCtx, subCtxCancel := context.WithCancel(ctx)
+		strm := ms.GetStream()
+		_ = s.server.ServeOne(subCtx, strm)
+		subCtxCancel()
+		strm.Close()
+		elRef.Release()
+	}()
+	return nil
+}
+
+// _ is a type assertion
+var _ link.MountedStreamHandler = ((*Server)(nil))
