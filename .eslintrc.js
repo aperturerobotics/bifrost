68ab3a72de5cabb0397ef1d4f06f5b6921bd14ec
Christian Stewart <christian@paral.in>
2022-07-08 18:13:11 -0700

    chore: use ts-common for config files
    Signed-off-by: Christian Stewart <christian@paral.in>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,1 @@
-module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-  }
-}
+./node_modules/@aperturerobotics/ts-common/.eslintrc.js
