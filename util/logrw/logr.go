4f06be9c040f19de4bac78000fe41dd98ade3a4b
Christian Stewart <christian@paral.in>
2022-09-27 19:02:17 -0700

    feat(logrw): add read/write debug logger
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ util/logrw/logr.go
@@ -0,0 +1,36 @@
+package logrw
+
+import (
+	"io"
+
+	"github.com/sirupsen/logrus"
+)
+
+// LogReader wraps a io.Reader with a logger.
+type LogReader struct {
+	le         *logrus.Entry
+	underlying io.Reader
+}
+
+func NewLogReader(le *logrus.Entry, underlying io.Reader) *LogReader {
+	return &LogReader{
+		le:         le,
+		underlying: underlying,
+	}
+}
+
+// Read reads data from the connection.
+// Read can be made to time out and return an error after a fixed
+// time limit; see SetDeadline and SetReadDeadline.
+func (c *LogReader) Read(b []byte) (n int, err error) {
+	n, err = c.underlying.Read(b)
+	if err != nil {
+		c.le.Warnf("read(...) => error %v", err.Error())
+	} else {
+		c.le.Debugf("read(...) => %v", string(b[:n]))
+	}
+	return
+}
+
+// _ is a type assertion
+var _ io.Reader = ((*LogReader)(nil))
---
+++ util/logrw/logw.go
@@ -0,0 +1,35 @@
+package logrw
+
+import (
+	"io"
+
+	"github.com/sirupsen/logrus"
+)
+
+// LogWriter wraps a io.Writer with a logger.
+type LogWriter struct {
+	le         *logrus.Entry
+	underlying io.Writer
+}
+
+func NewLogWriter(le *logrus.Entry, underlying io.Writer) *LogWriter {
+	return &LogWriter{
+		le:         le,
+		underlying: underlying,
+	}
+}
+
+// Write writes data to the connection.
+// Write can be made to time out and return an error after a fixed
+// time limit; see SetDeadline and SetWriteDeadline.
+func (c *LogWriter) Write(b []byte) (n int, err error) {
+	c.le.Debugf("write(%d): %v", len(b), string(b))
+	n, err = c.underlying.Write(b)
+	if err != nil {
+		c.le.Warnf("write(%d) => errored %v", len(b), err.Error())
+	}
+	return
+}
+
+// _ is a type assertion
+var _ io.Writer = ((*LogWriter)(nil))
