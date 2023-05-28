6ea068ab394914647e68d48fb20618ce6c4489a4
Christian Stewart <christian@aperture.us>
2023-05-27 17:50:25 -0700

    fix: allow passing nil le to http logger
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- http/log/client.go
+++ http/log/client.go
@@ -9,25 +9,30 @@ import (
 // DoRequest performs a request with logging.
 //
 // If verbose=true, logs successful cases as well as errors.
+// le can be nil to disable logging
 func DoRequest(le *logrus.Entry, client *http.Client, req *http.Request, verbose bool) (*http.Response, error) {
 	// Request details
-	le = le.WithFields(logrus.Fields{
-		"method": req.Method,
-		"url":    req.URL.String(),
-	})
-	if verbose {
-		le.Debug("starting request")
+	if le != nil {
+		le = le.WithFields(logrus.Fields{
+			"method": req.Method,
+			"url":    req.URL.String(),
+		})
+		if verbose {
+			le.Debug("starting request")
+		}
 	}
 	resp, err := client.Do(req)
-	if resp != nil {
-		le = le.WithField("status", resp.StatusCode)
-	}
-	if err != nil {
-		le.WithError(err).Warn("request errored")
-	} else if resp.StatusCode >= 400 {
-		le.Warn("request failed")
-	} else if verbose {
-		le.Debug("request succeeded")
+	if le != nil {
+		if resp != nil {
+			le = le.WithField("status", resp.StatusCode)
+		}
+		if err != nil {
+			le.WithError(err).Warn("request errored")
+		} else if resp.StatusCode >= 400 {
+			le.Warn("request failed")
+		} else if verbose {
+			le.Debug("request succeeded")
+		}
 	}
 	return resp, err
 }
