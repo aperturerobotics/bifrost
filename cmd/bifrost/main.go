172821ef1d72fb0f607f38fee3aa24535e32fc74
Christian Stewart <christian@aperture.us>
2024-01-22 23:43:18 -0800

    chore: include version in release binaries
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- cmd/bifrost/main.go
+++ cmd/bifrost/main.go
@@ -10,12 +10,20 @@ import (
 // Commands are the CLI commands
 var commands []*cli.Command
 
+var version string
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "bifrost"
 	app.HideVersion = true
 	app.Usage = "command-line node and tools for bifrost"
 	app.Commands = commands
+
+	if version == "" {
+		app.HideVersion = true
+	} else {
+		app.Version = version
+	}
 
 	if err := app.Run(os.Args); err != nil {
 		fmt.Println(err.Error())
