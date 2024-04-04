d90acff6d91a0742a3f7ff5f2eb2accec36224bc
Christian Stewart <christian@aperture.us>
2024-04-04 15:50:26 -0700

    feat: add http logger for fetch api
    Signed-off-by: Christian Stewart <christian@aperture.us>

---
+++ http/log/fetch/fetch_js.go
@@ -0,0 +1,70 @@
+//go:build js
+
+package httplog_fetch
+
+import (
+	"slices"
+	"strings"
+
+	fetch "github.com/aperturerobotics/bifrost/util/js-fetch"
+	"github.com/sirupsen/logrus"
+)
+
+var logHeaders = []string{"range", "content-range", "content-type", "content-length", "accept"}
+
+// Fetch uses the JS Fetch API to make requests with logging.
+//
+// if le is nil, all logging is disabled.
+// if verbose is set, both successful and failed calls are logged.
+func Fetch(le *logrus.Entry, url string, opts *fetch.Opts, verbose bool) (*fetch.Response, error) {
+	// Request details
+	if le != nil {
+		method := "GET"
+		if opts != nil && opts.Method != "" {
+			method = opts.Method
+		}
+
+		le = le.WithFields(logrus.Fields{
+			"method": method,
+			"url":    url,
+		})
+
+		if opts != nil && opts.Headers != nil {
+			// Parse and log some headers from the request
+			for hdr, hdrVal := range opts.Headers {
+				hdr = strings.ToLower(hdr)
+				if slices.Contains(logHeaders, hdr) {
+					le = le.WithField(hdr, hdrVal)
+				}
+			}
+		}
+
+		if verbose {
+			le.Debug("starting request")
+		}
+	}
+
+	resp, err := fetch.Fetch(url, opts)
+
+	if le != nil {
+		if resp != nil {
+			le = le.WithField("status", resp.Status)
+			for hdr, hdrVal := range resp.Headers {
+				hdr = strings.ToLower(hdr)
+				if slices.Contains(logHeaders, hdr) {
+					le = le.WithField(hdr, hdrVal)
+				}
+			}
+		}
+
+		if err != nil {
+			le.WithError(err).Warn("request errored")
+		} else if resp.Status >= 400 {
+			le.Warn("request failed")
+		} else if verbose {
+			le.Debug("request succeeded")
+		}
+	}
+
+	return resp, err
+}
