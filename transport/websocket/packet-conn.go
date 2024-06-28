e46f83a650fd797f1da6af277fc1629648c17a9e
Christian Stewart <christian@aperture.us>
2024-06-28 00:51:54 -0700

    fix: detect EOF error in websocket reader
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- transport/websocket/packet-conn.go
+++ transport/websocket/packet-conn.go
@@ -56,6 +56,14 @@ func (c *PacketConn) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
 	if err != nil {
 		if err == context.Canceled {
 			err = io.EOF
+		} else {
+			// Detect EOF wrapped with "failed to get reader"
+			for werr := errors.Unwrap(err); werr != nil; werr = errors.Unwrap(err) {
+				if werr == io.EOF || err == context.Canceled {
+					err = io.EOF
+					break
+				}
+			}
 		}
 		return 0, nil, err
 	}
